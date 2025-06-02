164be5448c969dc075143cb0d58685713e8e96e3
unknown <z5532071@ad.unsw.edu.au>
2025-06-02 15:26:58 +0800

    finish global.css file

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const page = () => {
   return (
-    <h1 className="text-3xl text-violet-700">
+    <h1 className="text-3xl text-light-500">
       Welcome to Next.jsnihaoooooooggggggggg
     </h1>
   );
