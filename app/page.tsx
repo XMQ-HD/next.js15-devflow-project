d4869225f144ff6e2b14e1450e245d2d6237bd21
unknown <z5532071@ad.unsw.edu.au>
2025-06-03 11:29:58 +0800

    set up light and dark theme and install shadcn and navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return (
-    <div>
-      <h1 className="h1-bold">Welcome to Next.jsnihaoooooooggggggggg</h1>
-      <h1 className="h1-bold font-inter">
-        Welcome to Next.jsnihaoooooooggggggggg
-      </h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to Next.jsnihaoooooooggggggggg
-      </h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;
