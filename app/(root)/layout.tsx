5af7b3a13e775c680a953761bd49abd15161f191
unknown <z5532071@ad.unsw.edu.au>
2025-06-05 16:00:38 +0800

    construct left and right Navsiderbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,23 @@
 import React, { ReactNode } from "react";
 
+import LeftSiderbar from "@/components/navigation/LeftSiderbar";
 import Navbar from "@/components/navigation/navbar";
+import RightSiderbar from "@/components/navigation/RightSiderbar";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <Navbar />
-      {children}
+
+      <div className="flex">
+        <LeftSiderbar />
+
+        <section className="flex min-h-screen flex-1  flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+
+        <RightSiderbar />
+      </div>
     </main>
   );
 };
--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,13 @@ const RootLayout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
+      <header>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
+      </header>
       <SessionProvider session={session}>
         <body
           className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
