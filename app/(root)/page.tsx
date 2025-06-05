647c891e3ff0ebc27095fb5e06852d4bcb5c22a4
unknown <z5532071@ad.unsw.edu.au>
2025-06-05 13:19:56 +0800

    create Mobile Navigation and Navlinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 const Home = async () => {
   const session = await auth();
 
   console.log("Session:", session);
-  return (
-    <div>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button typeof="submit">Logo out</Button>
-      </form>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
