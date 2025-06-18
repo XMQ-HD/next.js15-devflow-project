a069d6b303f451999eed11f23f2ce3294695e31f
unknown <z5532071@ad.unsw.edu.au>
2025-06-18 14:48:51 +0800

    Implement Sign In function

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { SignUpSchema } from "../validations";
+import { SignInSchema, SignUpSchema } from "../validations";
 import action from "../handles/action";
 import handleError from "../handles/error";
 import mongoose from "mongoose";
@@ -9,6 +9,7 @@ import Account from "@/database/account.model";
 import bcrypt from "bcryptjs";
 import { signIn } from "@/auth";
 import { ActionResponse, ErrorResponse } from "@/types/global";
+import { NotFoundError } from "../http-errors";
 
 export async function signUpWithCredentials(
   params: AuthCredentials
@@ -67,5 +68,45 @@ export async function signUpWithCredentials(
     return handleError(error) as ErrorResponse;
   } finally {
     await session.endSession();
+  }
+}
+
+export async function signInWithCredentials(
+  params: Pick<AuthCredentials, "email" | "password">
+): Promise<ActionResponse> {
+  const validationResult = await action({ params, schema: SignInSchema });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { email, password } = validationResult.params!;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new NotFoundError("User");
+    }
+
+    const existingAccount = await Account.findOne({
+      provider: "credentials",
+      providerAccountId: email,
+    });
+
+    if (!existingAccount) throw new NotFoundError("Account");
+
+    const passwordMatch = await bcrypt.compare(
+      password,
+      existingAccount.password
+    );
+
+    if (!passwordMatch) throw new Error("Paaword does not match.");
+
+    await signIn("credentials", { email, password, redirect: false });
+
+    return { success: true };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
   }
 }
