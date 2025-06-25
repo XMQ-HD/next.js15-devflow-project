5c7cfb0eba0188fb5bbfc4ad20e55448dd154835
unknown <z5532071@ad.unsw.edu.au>
2025-06-25 14:25:46 +0800

    Increment Question Details views - client side useEffect methods

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -5,6 +5,7 @@ import {
   AskQuestionSchema,
   EditQuestionSchema,
   GetQuestionSchema,
+  IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
 import Question, { IQuestionDoc } from "@/database/question.model";
@@ -22,7 +23,11 @@ import {
   CreateQuestionParams,
   EditQuestionParams,
   GetQuestionParams,
+  IncrementViewsParams,
 } from "@/types/action";
+import { number } from "zod";
+import { revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
 
 export async function createQuestion(
   params: CreateQuestionParams
@@ -301,6 +306,43 @@ export async function getQuestions(
     return {
       success: true,
       data: { questions: JSON.parse(JSON.stringify(questions)), isNext },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function incrementViews(
+  params: IncrementViewsParams
+): Promise<ActionResponse<{ views: number }>> {
+  const validationResult = await action({
+    params,
+    schema: IncrementViewsSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+
+  try {
+    const question = await Question.findById(questionId);
+
+    if (!question) {
+      throw new NotFoundError("Question not found.");
+    }
+
+    question.views += 1;
+
+    await question.save();
+
+    // revalidate the data for a specific path make sure the user see the most up-to-date information without delay
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+      success: true,
+      data: { views: question.views },
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
