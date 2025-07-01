735d51cce2809781c1601e3daaf7206c26436768
unknown <z5532071@ad.unsw.edu.au>
2025-07-01 21:45:53 +0800

    Show top tags at RightSiderbar

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -13,6 +13,7 @@ import {
 } from "@/types/global";
 import { GetTagQuestionsParams } from "@/types/action";
 import { NotFoundError } from "../http-errors";
+import dbConnect from "../mongoose";
 
 export const getTags = async (
   params: PaginatedSearchParams
@@ -158,6 +159,21 @@ export const getTagQuestions = async (
         questions: JSON.parse(JSON.stringify(questions)),
         isNext,
       },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+};
+
+export const getTopTags = async (): Promise<ActionResponse<Tag[]>> => {
+  try {
+    await dbConnect();
+
+    const tags = await Tag.find().sort({ questions: -1 }).limit(5);
+
+    return {
+      success: true,
+      data: JSON.parse(JSON.stringify(tags)),
     };
   } catch (error) {
     return handleError(error) as ErrorResponse;
