26867c6c2f9267f608b88076404e2e9b5a825d51
unknown <z5532071@ad.unsw.edu.au>
2025-07-09 19:23:52 +0800

    fix mongoose problem

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,5 +1,3 @@
-import { InteractionActionEnums } from "@/database/interaction.model";
-import Question from "@/database/question.model";
 import { z } from "zod";
 
 export const SignInSchema = z.object({
@@ -222,7 +220,16 @@ export const DeleteAnswerSchema = z.object({
 });
 
 export const CreateInteractionSchema = z.object({
-  action: z.enum(InteractionActionEnums),
+  action: z.enum([
+    "view",
+    "upvote",
+    "downvote",
+    "bookmark",
+    "post",
+    "edit",
+    "delete",
+    "search",
+  ]),
   actionTarget: z.enum(["question", "answer"]),
   actionId: z.string().min(1),
   authorId: z.string().min(1),
