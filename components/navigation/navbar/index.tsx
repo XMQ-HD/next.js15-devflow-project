f8244e0d38482c8db43eda10268d4ade9bd49f95
unknown <z5532071@ad.unsw.edu.au>
2025-07-09 14:51:01 +0800

    Implement Global search

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -6,13 +6,15 @@ import MobileNavigation from "./MobileNavigation";
 import Theme from "./Theme";
 import { auth } from "@/auth";
 import UserAvatar from "@/components/UserAvatar";
+import ROUTES from "@/constants/routes";
+import GlobalSearch from "@/components/search/GlobalSearch";
 
 const Navbar = async () => {
   const session = await auth();
 
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 shadow-light-300 gap-5">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href={ROUTES.HOME} className="flex items-center gap-1">
         <Image
           src="/images/site-logo.svg"
           width={23}
@@ -21,11 +23,11 @@ const Navbar = async () => {
         />
 
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
-          Dev<span className="text-primary-500">Flow</span>
+          Dev<span className="text-primary-500">DevFlow</span>
         </p>
       </Link>
 
-      <p>Global Search</p>
+      <GlobalSearch />
 
       <div className="flex-between gap-5">
         <Theme />
